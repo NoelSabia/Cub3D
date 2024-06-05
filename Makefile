1e9f514e2612ba7caf2f07e2483de55695c35ca6
Z1nnlos <Noel.Sabia@gmx.net>
2024-06-05 02:26:22 +0200

    first commit with structure finished

--- Makefile
+++ Makefile
@@ -1,44 +1,37 @@
-NAME	:= Cub3D
+NAME := Cub3D
 
-CFLAGS	:= -Wextra -Wall -Werror
+CFLAGS = -Wextra -Wall -Werror
 
-HEADERS	:= -I ./include
+HEADERS := -I ./include
 
 SRCDIR := ./src/
-
-SRCS	:=  $(SRCDIR)main.c
+SRCS := $(SRCDIR)main.c
 
 OBJDIR := ./obj/
-OBJS	:= $(SRCS:.c=.o)
-OBJS	:= $(addprefix $(OBJDIR),$(OBJS))
+OBJS := $(SRCS:$(SRCDIR)%.c=$(OBJDIR)%.o)
 
-CC		:= cc
+CC := cc
 
-.PHONY: all, clean, fclean, re
+.PHONY: all clean fclean re
 
 all: $(NAME)
 
 libft:
-	@make -C ./lib/libft
+	@make -C libft
 
-get_next_line:
-	@make -C ./lib/get_next_line
-
-$(OBJDIR)%.o: %.c
+$(OBJDIR)%.o: $(SRCDIR)%.c
 	mkdir -p $(@D)
 	$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS)
 
-$(NAME): $(OBJS) libft get_next_line
-	$(CC) $(OBJS) ./lib/libft/libft.a ./lib/get_next_line/get_next_line.a $(HEADERS) -lreadline -lncurses -ltermcap -o $(NAME)
+$(NAME): $(OBJS) libft
+	$(CC) $(OBJS) -o $(NAME) $(HEADERS)
 
 clean:
-	@make -C ./lib/libft clean
-	@make -C ./lib/get_next_line clean
+	@make -C libft clean
 	@rm -rf $(OBJDIR)
 
 fclean: clean
-	@make -C ./lib/libft fclean
-	@make -C ./lib/get_next_line fclean
+	@make -C libft fclean
 	@rm -rf $(NAME)
 
 re: fclean all
--- src/includes/cub3d.h
+++ src/includes/cub3d.h
@@ -1,6 +1,17 @@
-#ifndef cub3d
-#define cub3d
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 01:07:58 by nsabia            #+#    #+#             */
+/*   Updated: 2024/06/05 01:42:17 by nsabia           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -12,6 +23,14 @@
 # include <stdbool.h>
 
 # include "../../libft/libft.h"
-# include "../../get_next_line/get_next_line.h"
+# include "../../MLX42/include/MLX42/MLX42.h"
+
+# define SCREEN_WIDTH 1900
+# define SCREEN_HEIGHT 1000
+# define TILE_SIZE 30
+# define FOV 60
+# define ROTATION_SPEED 0.045
+# define PLAYER_SPEED 4
+
 
 #endif
