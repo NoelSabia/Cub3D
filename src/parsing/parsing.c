c921a8e70319715edd29742abe3e15ad59b4a0bf
Z1nnlos <Noel.Sabia@gmx.net>
2024-06-05 02:53:23 +0200

    safty push cause im tired

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,22 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/05 00:47:44 by nsabia            #+#    #+#             */
-/*   Updated: 2024/06/05 02:34:21 by nsabia           ###   ########.fr       */
+/*   Created: 2024/06/05 02:31:36 by nsabia            #+#    #+#             */
+/*   Updated: 2024/06/05 02:32:06 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./includes/cub3d.h"
+#include "../includes/cub3d.h"
 
-int	main(int argc, char *argv[])
-{
-	if (argc < 1 || argc > 1)
-		printf("Usage: ./Cub3d <filename>.cub");
-	else
-		parsing();
-	return (0);
-}
+
