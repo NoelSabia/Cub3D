d4143dec0b3f57d70958e561ee45c0a2588d72da
Z1nnlos <Noel.Sabia@gmx.net>
2024-06-05 18:22:06 +0200

    reading the input kinda works just some slight problem with the format

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,21 +6,38 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 02:31:36 by nsabia            #+#    #+#             */
-/*   Updated: 2024/06/05 15:33:07 by nsabia           ###   ########.fr       */
+/*   Updated: 2024/06/05 18:21:10 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	read_into_input(t_parsing *parse, char *filename)
+char	*read_into_input(char *filename)
 {
-	int	fd;
+	int		fd;
+	char	*buffer;
+	int		size;
+	int		i;
 
-	(void)parse;
+	size = 1000000;
+	buffer = ft_calloc(1, size);
+	i = 0;
 	fd = open(filename, O_RDONLY);
+	while (read(fd, buffer, size))
+	{
+		buffer = ft_realloc(buffer, size, size + 100);
+		size = size + 100;
+		i = size;
+	}
+	buffer[size] = '\0';
+	close(fd);
+	return (buffer);
 }
 
 void	parsing(t_parsing *parse, char *filename)
 {
-	read_into_input(parse, filename);
+	char	*result;
+
+	(void)parse;
+	result = read_into_input(filename);
 }
